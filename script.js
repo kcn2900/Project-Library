4550395fe6d800631df06373e5c0f4ce6d643fd2
kcn2900 <nguyenkevin2900@gmail.com>
2024-06-29 05:03:02 -0500

    Provide more CSS changes and add basic pop-up when hovering over cards in js.

--- script.js
+++ script.js
@@ -6,11 +6,30 @@ const newBookBtn = document.querySelector(".new");
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.pages = +pages;
     this.readStatus = readStatus;
 
-    this.info = () => {
+    this.info = (showcase_all) => {
+        let title_result = this.title;
+        let author_result = this.author;
+        let pages_result = this.pages;
         let statusText = "";
+
+        // check for test if wanting to showcase entire book info
+        if (typeof showcase_all === "undefined") {
+            if (this.title.length > 10) {
+                title_result = title.slice(0, 10) + "...";
+            }
+    
+            if (this.author.length > 10) {
+                author_result = author.slice(0, 10) + "...";
+            }
+    
+            if(this.pages > 10000) {
+                pages_result = pages.toString().slice(0, 5) + "+";
+            }
+        }
+
         switch (+this.readStatus) {
             case 1:
                 statusText = "Reading";
@@ -21,7 +40,9 @@ function Book(title, author, pages, readStatus) {
             default:
                 statusText = "Not Yet Reading";
         }
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${statusText}`;
+
+        return `${title_result} by ${author_result}, 
+        ${pages_result} pages, ${statusText}`;
     };
 }
 
@@ -37,65 +58,100 @@ function displayLibrary () {
         newCard.className = "card";
         newCard.textContent = myLibrary[i].info();
 
-        console.log(newCard.textContent);
-        console.log(myLibrary[i]);
+        // console.log(newCard.textContent);
+        // console.log(myLibrary[i]);
 
         let removeBtn = document.createElement("button");
         removeBtn.textContent = "REMOVE"
         removeBtn.className = "remove";
         removeBtn.id = +i;
 
-
         let statusBtn = document.createElement("button");
         statusBtn.textContent = "READ";
         statusBtn.className = "status-button";
         statusBtn.id = +i;
 
-        newCard.appendChild(removeBtn);
-        newCard.appendChild(statusBtn);
+        let buttonSection = document.createElement("div");
+        buttonSection.className = "button-section";
+        buttonSection.append(removeBtn, statusBtn);
+
+        newCard.appendChild(buttonSection);
         gridDiv.appendChild(newCard);
 
+        // remove card based on myLibrary index
+        // 'reload' or redo the library display to update indexes
         removeBtn.addEventListener('click', (e) => {
-            console.log(e.target.id);
+            // console.log(e.target.id);
             let index = +e.target.id;
 
-            console.log(gridDiv.children[index]);
+            // console.log(gridDiv.children[index]);
 
             gridDiv.removeChild(gridDiv.children[index]);
-            console.log(myLibrary.splice(index, 1));
+            myLibrary.splice(index, 1);
             
             displayLibrary();
         });
 
+        // set a book's status in myLibrary to "Reading"
+        // and reload display to showcase the text change
         statusBtn.addEventListener('click', (e) => {
             let index = +e.target.id;
 
-            console.log("Before: " + myLibrary[index].readStatus);
+            // console.log("Before: " + myLibrary[index].readStatus);
 
             myLibrary[index].readStatus = 1;
 
-            console.log("After: " + myLibrary[index].readStatus);
+            // console.log("After: " + myLibrary[index].readStatus);
 
             // gridDiv.children[index].textContent = myLibrary[index].info();
             displayLibrary();
-        })
+        });
+
+        newCard.addEventListener('mouseover', () => {
+            // add a new card with an abolute position, but
+            // make the card's position relative
+
+            hoverCard = document.createElement("div");
+            hoverCard.textContent = myLibrary[i].info(true);
+            hoverCard.className = "hover-card";
+
+            gridDiv.appendChild(hoverCard);
+
+        });
+
+        newCard.addEventListener('mouseout', () => {
+            gridDiv.removeChild(hoverCard);
+        });
     }
 }
 
-// let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
-// let randomBook = new Book("Random Book", "Unknown", 999, 1);
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
+let randomBook = new Book("Random Book", "Unknown", 999, 1);
 
-// addBookToLibrary(theHobbit);
-// addBookToLibrary(randomBook);
+addBookToLibrary(theHobbit);
+addBookToLibrary(randomBook);
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    let bookTitle = document.querySelector("#title");
+    let bookAuthor = document.querySelector("#author");
+    let bookPages =  document.querySelector("#page-number");
+    let bookStatus = document.querySelector("#status");
+
     let newBook = new Book(
-        document.querySelector("#title").value,
-        document.querySelector("#author").value,
-        document.querySelector("#page-number").value,
-        document.querySelector("#status").value
+        bookTitle.value,
+        bookAuthor.value,
+        bookPages.value,
+        bookStatus.value
     );
+
+    // could agrubaly use a forEach/for loop but since it's a small
+    // number of inputs, don't really need to do so.
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+    bookStatus.value = "";
 
     addBookToLibrary(newBook);
     displayLibrary();
@@ -104,9 +160,7 @@ form.addEventListener('submit', (e) => {
 });
 
 newBookBtn.addEventListener('click', () => {
-    form.style.display = "block";
+    form.style.display = "flex";
     newBookBtn.style.display = "none";
 });
-
-
 
